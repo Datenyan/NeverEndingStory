3db266a85746064b92d556899703f4e4c7f82f18
drai0001 <13450157+Datenyan@users.noreply.github.com>
2020-11-22 00:01:59 +1100

    Basic functionality in place

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
+// Imports
 let express = require("express");
-var router = require("./router.js");
+let bodyParser = require("body-parser"); 
+let flash = require("connect-flash"); // for flash messages 
+let session = require('express-session'); // for flash messages
+const { exit } = require("process");
 let app = express();
 
+// Check args for secret key
+let args = process.argv.slice(2);
+if (args.length < 1) {
+    console.log("You must provide a secret key for flash-messages!")
+    exit(1);
+} else {
+    var secretFlashKey = '' + args[1]; // ensure that the string is considered a json value rather than a string
+}
+
+// Set up session for flash messsages
+app.use(session({
+    secret: secretFlashKey,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
+
+// Configure imports
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+app.use(bodyParser.urlencoded({extended: false})) 
 app.use(express.static('res'));
-app.use("/", router);
 
+// Setup router for endpoints
+var router = require("./router.js");
+app.use("/", router); 
+
+// Run the server 
 app.listen("8080");
 console.log("Server running at http://127.0.0.1:8080");
--- router.js
+++ router.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-var index = router.get('/', function(req, res) {
-    res.send('The webserver is working!');
+var story = "Testing 123";
+var suggestedWords = [];
+
+// Helper function to get current story text
+var getStory = function() {
+    return story;
+}
+
+// Display homepage
+router.get('/', function(req, res) {
+    let storydata = getStory();
+    res.render("index.html", {story: storydata, words: suggestedWords, flash: req.flash('msg')});
+});
+
+router.post('/suggestword', function(req, res) {
+    let newWord = req.body.word;
+    //!TODO - ensure only one word and return error
+    try {
+        suggestedWords.push({
+            'name': newWord,
+            'votes': 0
+        });
+        req.flash('msg', 'Your word has been submitted!');
+        console.log(suggestedWords);
+    } catch (e) {
+        console.log(e);
+        req.flash('msg', 'Your word was unable to be submitted.')
+    }
+    res.redirect('/');
 });
 
 module.exports = router;
