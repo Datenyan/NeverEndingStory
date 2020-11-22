34e13107c7567d9a2adf39c76ef764ca98b11be4
drai0001 <13450157+Datenyan@users.noreply.github.com>
2020-11-22 23:23:36 +1100

    Added validation to suggest word functionality

--- router.js
+++ router.js
@@ -1,33 +1,88 @@
 var express = require('express');
+const { exit } = require('process');
 var router = express.Router();
 
 var story = "Testing 123";
 var suggestedWords = [];
+var suggestedWordId = 0;
 
-// Helper function to get current story text
+/*
+    HELPER FUNCTIONS
+*/
+
+// Returns the current story
 var getStory = function() {
     return story;
 }
 
-// Display homepage
+// Adds a word to the suggested word list
+var addSuggestedWord = function(newWord) {
+    try {
+        suggestedWords.push({
+            'id': suggestedWordId++,
+            'name': newWord,
+            'votes': 0
+        });
+        return true;
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
+}
+
+// Checks if a "word" has non-alphanumeric characters
+var checkWordAlphanumeric = function(word) {
+    return word.match(/[^a-z0-9]/gi);
+}
+
+// Removes all non-alphanumeric characters from a "word"
+var stripWord = function(word) { 
+    return word.replace(/[^a-z0-9]/gi, "");
+}
+
+var checkSuggestedWord = function(word) {
+    let retVal = false;
+    for (let i = 0; i < suggestedWords.length; i++) {
+        if (suggestedWords[i].name == word) {
+            retVal = true;
+        }
+    }
+    return retVal;
+}
+
+/*
+    ROUTER ENDPOINTS
+*/
+
+// Homepage
 router.get('/', function(req, res) {
     let storydata = getStory();
     res.render("index.html", {story: storydata, words: suggestedWords, flash: req.flash('msg')});
 });
 
+// POST Request for new suggested word
+// !TODO - Clean up this mess
 router.post('/suggestword', function(req, res) {
     let newWord = req.body.word;
-    //!TODO - ensure only one word and return error
-    try {
-        suggestedWords.push({
-            'name': newWord,
-            'votes': 0
-        });
-        req.flash('msg', 'Your word has been submitted!');
-        console.log(suggestedWords);
-    } catch (e) {
-        console.log(e);
-        req.flash('msg', 'Your word was unable to be submitted.')
+    // If the word contains spaces then automatically fail the word adding
+    if (newWord.indexOf(" ") != -1) {
+        req.flash('msg', 'Please only use one word, and no spaces.');
+    } else {
+        // If the word contained any non-alphanumeric characters 
+        if (checkWordAlphanumeric(newWord)) {
+            newWord = stripWord(newWord);
+            req.flash('msg', 'Non-alphanumeric characters were stripped from your word.');
+        }
+        // Attempt to add word
+        if (!checkSuggestedWord(newWord) && newWord != "") {
+            if (addSuggestedWord(newWord)) {
+                req.flash('msg', 'Your word has been submitted!');
+            } else {
+                req.flash('msg', 'Your word was unable to be submitted.');
+            }
+        } else {
+            req.flash('msg', 'You can\'t submit an empty string. Please enter a word.');
+        }
     }
     res.redirect('/');
 });
