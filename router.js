18f054fd55717f1b18c87e977d98be3222b0602d
drai0001 <13450157+Datenyan@users.noreply.github.com>
2020-11-28 19:44:55 +1100

    Cleaned up the suggest word checks for readability

--- router.js
+++ router.js
@@ -15,6 +15,28 @@ var getStory = function() {
     return story;
 }
 
+// Checks and cleans a suggested word
+var checkSuggestedWord = function(newWord, req) {
+    // If the word is empty (i.e. ""), then fail the word adding
+    if (newWord == "") {
+        req.flash('msg', 'You can\'t submit an empty string. Please enter a word.');
+        return false;
+    }
+    // If the word contains spaces then automatically fail the word adding
+    if (newWord.indexOf(" ") != -1) {
+        req.flash('msg', 'Please only use one word, and no spaces.');
+        return false;
+    }
+    // If the word already exists in the system then fail adding it 
+    if (checkSuggestedWordExistsInList(newWord)) {
+        req.flash('msg', newWord + ' already exists in the list of suggested words.');
+        return false;
+    }
+
+    // If all checks pass, then return true.
+    return true;
+}
+
 // Adds a word to the suggested word list
 var addSuggestedWord = function(newWord) {
     try {
@@ -40,10 +62,11 @@ var stripWord = function(word) {
     return word.replace(/[^a-z0-9]/gi, "");
 }
 
-var checkSuggestedWord = function(word) {
+// Checks if the suggested word already exists in the list of suggested words
+var checkSuggestedWordExistsInList = function(word) {
     let retVal = false;
     for (let i = 0; i < suggestedWords.length; i++) {
-        if (suggestedWords[i].name == word) {
+        if (suggestedWords[i].name.toLowerCase() == word.toLowerCase()) {
             retVal = true;
         }
     }
@@ -61,27 +84,21 @@ router.get('/', function(req, res) {
 });
 
 // POST Request for new suggested word
-// !TODO - Clean up this mess
 router.post('/suggestword', function(req, res) {
     let newWord = req.body.word;
-    // If the word contains spaces then automatically fail the word adding
-    if (newWord.indexOf(" ") != -1) {
-        req.flash('msg', 'Please only use one word, and no spaces.');
-    } else {
-        // If the word contained any non-alphanumeric characters 
-        if (checkWordAlphanumeric(newWord)) {
-            newWord = stripWord(newWord);
-            req.flash('msg', 'Non-alphanumeric characters were stripped from your word.');
-        }
-        // Attempt to add word
-        if (!checkSuggestedWord(newWord) && newWord != "") {
-            if (addSuggestedWord(newWord)) {
-                req.flash('msg', 'Your word has been submitted!');
-            } else {
-                req.flash('msg', 'Your word was unable to be submitted.');
-            }
+
+    // Remove non-alphanumeric characters from the suggested "word"
+    if (checkWordAlphanumeric(newWord)) {
+        newWord = stripWord(newWord); // ensure no non-alphanumeric characters
+        req.flash('msg', 'Non-alphanumeric characters were stripped from your word.')
+    }
+
+    // Ensure the suggested "word" passes checks before attempting to add  
+    if (checkSuggestedWord(newWord, req)) { 
+        if (addSuggestedWord(newWord)) {
+            req.flash('msg', 'Your word has been submitted!');
         } else {
-            req.flash('msg', 'You can\'t submit an empty string. Please enter a word.');
+            req.flash('msg', 'Your word was unable to be submitted as an error occurred.');
         }
     }
     res.redirect('/');
